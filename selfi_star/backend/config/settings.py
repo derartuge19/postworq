1c2f7ea043f46e176d86beadf8f43d6e5046b8ae
derartuge19 <derartuge@gmail.com>
2026-04-01 11:59:59 +0300

    Update Neon database connection to use new pooler host

--- selfi_star/backend/config/settings.py
+++ selfi_star/backend/config/settings.py
@@ -100,7 +100,7 @@ else:
             'NAME': config('DB_NAME', default='neondb'),
             'USER': config('DB_USER', default='neondb_owner'),
             'PASSWORD': config('DB_PASSWORD', default='npg_gQpuHj7IBoC1'),
-            'HOST': config('DB_HOST', default='ep-flat-thunder-a5790996.us-east-2.aws.neon.tech'),
+            'HOST': config('DB_HOST', default='ep-rough-math-anwwqd2n-pooler.c-6.us-east-1.aws.neon.tech'),
             'PORT': config('DB_PORT', default='5432'),
             'OPTIONS': {
                 'sslmode': 'require',
