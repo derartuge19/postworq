d890dcc9822eb09b894105c2538ae818a4d4831c
derartuge19 <derartuge@gmail.com>
2026-04-07 12:03:14 +0300

    Add CampaignScoringConfig stub to fix Vercel cache import error

---
+++ selfi_star/src/admin/pages/CampaignScoringConfig.jsx
@@ -0,0 +1,2 @@
+// Compatibility re-export - Vercel cache may reference this old filename
+export { TypeSpecificScoringConfig as default, TypeSpecificScoringConfig } from './TypeSpecificScoringConfig';
