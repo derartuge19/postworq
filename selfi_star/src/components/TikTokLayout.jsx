11f5c61bde6464bbb3f9b9e47194ac61f0036cf1
derartuge19 <derartuge@gmail.com>
2026-04-20 22:37:03 +0300

    Add autoPlay attribute to reel page videos
    - Add autoPlay to video element in TikTokLayout
    - Videos will now play automatically when page loads

--- selfi_star/src/components/TikTokLayout.jsx
+++ selfi_star/src/components/TikTokLayout.jsx
@@ -1810,6 +1810,7 @@ export const TikTokLayout = memo(function TikTokLayout({
                               ? 'metadata'
                               : 'none'
                           }
+                          autoPlay
                           loop
                           playsInline
                           onLoadedMetadata={(e) => {
