45b578eec5f338edb9f779696c2597b007700de8
derartuge19 <derartuge@gmail.com>
2026-04-24 09:36:38 +0300

    Fix withKotlinVersionFix plugin type handling
    - Add type checking for buildGradle before calling string methods
    - Properly access config.modResults structure
    - Return config object after modifications

--- mobile-app/plugins/withKotlinVersionFix.js
+++ mobile-app/plugins/withKotlinVersionFix.js
@@ -1,36 +1,40 @@
 const { withProjectBuildGradle, withAppBuildGradle } = require('@expo/config-plugins');
 
-function setKotlinVersion(buildGradle) {
+function setKotlinVersion(config) {
+  let buildGradle = config.modResults;
+  
+  if (typeof buildGradle !== 'string') {
+    return config;
+  }
+
   if (buildGradle.includes('kotlinVersion')) {
-    return buildGradle.replace(
+    buildGradle = buildGradle.replace(
       /ext\.kotlinVersion\s*=\s*["'].*?["']/,
       "ext.kotlinVersion = '1.9.25'"
     );
+  } else {
+    // Add kotlinVersion if it doesn't exist
+    const buildscriptMatch = buildGradle.match(/buildscript\s*\{/);
+    if (buildscriptMatch) {
+      const insertPosition = buildGradle.indexOf(buildscriptMatch[0]) + buildscriptMatch[0].length;
+      buildGradle =
+        buildGradle.slice(0, insertPosition) +
+        "\n    ext.kotlinVersion = '1.9.25'" +
+        buildGradle.slice(insertPosition);
+    }
   }
-  
-  // Add kotlinVersion if it doesn't exist
-  const buildscriptMatch = buildGradle.match(/buildscript\s*\{/);
-  if (buildscriptMatch) {
-    const insertPosition = buildGradle.indexOf(buildscriptMatch[0]) + buildscriptMatch[0].length;
-    return (
-      buildGradle.slice(0, insertPosition) +
-      "\n    ext.kotlinVersion = '1.9.25'" +
-      buildGradle.slice(insertPosition)
-    );
-  }
-  
-  return buildGradle;
+
+  config.modResults = buildGradle;
+  return config;
 }
 
 module.exports = function withKotlinVersionFix(config) {
   config = withProjectBuildGradle(config, (config) => {
-    config.modResults = setKotlinVersion(config.modResults);
-    return config;
+    return setKotlinVersion(config);
   });
 
   config = withAppBuildGradle(config, (config) => {
-    config.modResults = setKotlinVersion(config.modResults);
-    return config;
+    return setKotlinVersion(config);
   });
 
   return config;
