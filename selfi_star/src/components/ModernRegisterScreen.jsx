089bacbcec31eb4a66ed39de66f69c43e28be09b
edenzewdu11 <edenzewdutadesse11@gmail.com>
2026-04-24 14:45:51 +0300

    Increase logo width and height
    - Change logo size from 40x40 to 60x60 pixels
    - Both ethio and flipstar logos now more visible
    - Applied to both login and registration pages

--- selfi_star/src/components/ModernRegisterScreen.jsx
+++ selfi_star/src/components/ModernRegisterScreen.jsx
@@ -92,7 +92,7 @@ export function ModernRegisterScreen({ onSuccess, onLogin, onBack }) {
         alignItems: "center",
         gap: 10,
       }}>
-        <img src={ethioLogo} alt="Ethio Telecom" style={{ width: 40, height: 40, objectFit: "contain" }} />
+        <img src={ethioLogo} alt="Ethio Telecom" style={{ width: 60, height: 60, objectFit: "contain" }} />
       </div>
       <div style={{
         position: "absolute",
@@ -101,7 +101,7 @@ export function ModernRegisterScreen({ onSuccess, onLogin, onBack }) {
         display: "flex",
         alignItems: "center",
       }}>
-        <img src={flipLogo} alt="FlipStar" style={{ width: 40, height: 40, objectFit: "contain" }} />
+        <img src={flipLogo} alt="FlipStar" style={{ width: 60, height: 60, objectFit: "contain" }} />
       </div>
 
       <div style={{
