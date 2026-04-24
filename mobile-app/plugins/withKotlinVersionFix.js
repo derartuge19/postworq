82701b0f2590cfe02956a90f7a14119d740a0905
derartuge19 <derartuge@gmail.com>
2026-04-24 09:42:49 +0300

    Simplify plugin to use withGradleProperties for kotlinVersion
    - Change from modifying build.gradle files to setting gradle.properties
    - Use withGradleProperties to set kotlinVersion = 1.9.25
    - This approach is more reliable for setting Kotlin version globally

--- mobile-app/plugins/withKotlinVersionFix.js
+++ mobile-app/plugins/withKotlinVersionFix.js
@@ -1,41 +1,20 @@
-const { withProjectBuildGradle, withAppBuildGradle } = require('@expo/config-plugins');
-
-function setKotlinVersion(config) {
-  let buildGradle = config.modResults;
-  
-  if (typeof buildGradle !== 'string') {
-    return config;
-  }
-
-  if (buildGradle.includes('kotlinVersion')) {
-    buildGradle = buildGradle.replace(
-      /ext\.kotlinVersion\s*=\s*["'].*?["']/,
-      "ext.kotlinVersion = '1.9.25'"
-    );
-  } else {
-    // Add kotlinVersion if it doesn't exist
-    const buildscriptMatch = buildGradle.match(/buildscript\s*\{/);
-    if (buildscriptMatch) {
-      const insertPosition = buildGradle.indexOf(buildscriptMatch[0]) + buildscriptMatch[0].length;
-      buildGradle =
-        buildGradle.slice(0, insertPosition) +
-        "\n    ext.kotlinVersion = '1.9.25'" +
-        buildGradle.slice(insertPosition);
-    }
-  }
-
-  config.modResults = buildGradle;
-  return config;
-}
+const { withGradleProperties } = require('@expo/config-plugins');
 
 module.exports = function withKotlinVersionFix(config) {
-  config = withProjectBuildGradle(config, (config) => {
-    return setKotlinVersion(config);
-  });
+  return withGradleProperties(config, (config) => {
+    config.modResults = config.modResults.filter((item) => {
+      if (item.type === 'property' && item.key === 'kotlinVersion') {
+        return false;
+      }
+      return true;
+    });
 
-  config = withAppBuildGradle(config, (config) => {
-    return setKotlinVersion(config);
-  });
+    config.modResults.push({
+      type: 'property',
+      key: 'kotlinVersion',
+      value: '1.9.25',
+    });
 
-  return config;
+    return config;
+  });
 };
