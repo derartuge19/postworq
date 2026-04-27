013d75cd87c041ddf87257ef15e0b17a5fcf6777
derartuge19 <derartuge@gmail.com>
2026-04-27 12:43:31 +0300

    fix: merge conflicting migrations 0043_phone_otp_password_reset and 0049_gift

---
+++ selfi_star/backend/api/migrations/0050_merge_0043_phone_otp_password_reset_0049_gift.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0043_phone_otp_password_reset'),
+        ('api', '0049_gift_icon_color_gift_icon_name_and_more'),
+    ]
+
+    operations = [
+    ]
